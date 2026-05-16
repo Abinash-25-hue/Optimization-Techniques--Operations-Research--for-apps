90f9420f9e0854f547d7bf449eb700fb3efb0fe7
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-05-16 19:11:20 +0530

    Created nwcm.java and nwcm.class
    This is the creation of nwcm.java program, this implements the 'North West Corner Method' in Optimization Techniques, in this commit the supply and demand arrays have been created along with two methods that work on the array.

---
+++ nwcm.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class nwcm
+{
+    static void filler (int array[], int element)
+    {
+        //int n = array.size();
+        for (int index = 0; index < array.length; index++)
+        {
+            array[index] = element;
+        }
+    }
+
+    static void array_printer (int array[])
+    {
+        for (int index = 0; index < array.length; index++)
+        {
+            System.out.print(array[index] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+
+        int supply, demand;
+        System.out.print("Enter the number of supply : ");
+        supply = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter the number of demand : ");
+        demand = sc.nextInt();
+        sc.nextLine();
+
+        int supply_array[] = new int [supply];
+        int demand_array[] = new int [demand];
+
+        filler(supply_array, 0);
+        filler(demand_array, 0);
+
+        array_printer(supply_array);
+        array_printer(demand_array);
+    }
+}
